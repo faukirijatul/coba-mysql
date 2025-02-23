966f950b6a75c12a0999b0086b2e35f6b1e65392
faukirijatul <faukirijatul42@gmail.com>
2025-02-23 07:46:47 +0700

    update

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -16,16 +16,19 @@ const sequelize = new Sequelize(
   dbConfig.password,
   {
     host: dbConfig.host,
-    dialect: dbConfig.dialect,
+    dialect: "mysql",
+    port: 3306,
     logging: false,
   }
 );
 
-try {
-  await sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 
 export default sequelize;
